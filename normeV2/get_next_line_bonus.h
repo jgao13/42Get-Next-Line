ea0a7ac845ea4ef96668fe1b3a7b364edd996063
jgao <jgao@student.42.fr>
2021-01-08 13:32:37 +0100

    create normeV3 and opti some parts of code

---
+++ normeV2/get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jgao <jgao@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/23 11:31:50 by jgao              #+#    #+#             */
+/*   Updated: 2020/11/27 15:29:01 by jgao             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+size_t	lenbuffer(int fd);
+size_t	ft_strlen(char *s);
+size_t	custom_strlen(char *s);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_realloc(void *ptr, size_t size);
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4096
+# endif
+
+#endif
